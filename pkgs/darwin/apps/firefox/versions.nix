8c85a17ec7e158842283686fd6260d3ffd7943a7
toonn <toonn@toonn.io>
2023-04-19 11:22:43 +0200

    Apps: Firefox 112.0 -> 112.0.1

--- pkgs/darwin/apps/firefox/versions.nix
+++ pkgs/darwin/apps/firefox/versions.nix
@@ -200,6 +200,9 @@ let versions = builtins.mapAttrs (n: v: v // { "version" = n; }) {
       "112.0" = {
         sha256 = "1023m4mlzhafkjzwkaz8ygwm41gvra4q7l6xmhf9vdj37kdv6v0f";
       };
+      "112.0.1" = {
+        sha256 = "07rvhbvjpib54qk6m5imh9dysb4zvxjr9lzg620n75w4c14jlm9j";
+      };
     };
     latestVersion = lib.lists.foldr (v: lV:
                                       if builtins.compareVersions v lV == 1
